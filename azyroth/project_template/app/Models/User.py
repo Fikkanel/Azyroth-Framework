8ca00b98ec99375a56975d796297332b0d26f692
El <mfikkanel@gmail.com>
2025-08-05 00:34:55 +0700

    Update User.py

--- azyroth/project_template/app/Models/User.py
+++ azyroth/project_template/app/Models/User.py
@@ -1,15 +1,16 @@
+from sqlalchemy.orm import declarative_base, relationship # Pastikan relationship diimpor
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
     email = Column(String(255), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
 
+    posts = relationship("Post", back_populates="user")
+
     def __repr__(self):
-        return f"<User(id={self.id}, name='{self.name}', email='{self.email}')>"
+        return f"<User(name='{self.name}')>"
