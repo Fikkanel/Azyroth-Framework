e8c9cc81d560bfbb0fe18230a17ce93f73d9aba7
El <mfikkanel@gmail.com>
2025-08-11 14:13:25 +0700

    Update UserSeeder.py

--- azyroth/project_template/database/seeders/UserSeeder.py
+++ azyroth/project_template/database/seeders/UserSeeder.py
@@ -1,13 +1,9 @@
 from app.Models.User import User
-# Hapus 'from flask import g' karena tidak lagi digunakan
 
 class UserSeeder:
-    def run(self, session): # <-- Terima 'session' sebagai argumen
+    def run(self, session):
         """Menjalankan database seeder untuk user."""
-        
-        # Hanya jalankan jika tabel user masih kosong
         if session.query(User).count() == 0:
-            # Buat user admin contoh
             admin_user = User(
                 name='Admin',
                 email='admin@example.com',
@@ -17,4 +13,4 @@ class UserSeeder:
             session.commit()
             print("Admin user has been created.")
         else:
-            print("Users table is not empty, skipping seeder.")
+            print("Users table is not empty, skipping UserSeeder.")
